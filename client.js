c7eddd166362c105ba4a35bb35a126e221dcfd00
Pouya Elishaee <p.elishaee@gmail.com>
2022-01-19 15:44:45 -0500

    final

---
+++ client.js
@@ -0,0 +1,22 @@
+const { Module } = require("module");
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "165.227.47.243",
+    port: "50541"
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on('connect', () => {
+    console.log("Successfully connected to game server!");
+  });
+
+
+  return conn;
+};
+
+Module.exports=connect ;
